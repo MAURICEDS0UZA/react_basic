536680352b883eec069e0186d84cbd2e7cdfa1ad
MAURICEDS0UZA <145907681+MAURICEDS0UZA@users.noreply.github.com>
2024-04-23 14:27:27 +0530

    Feat: Import Hero component into App.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-import Navbar from './Components/N'
+import Navbar from './Components/Navbar'
+import Hero from './Components/Hero'
 
 export default function App() {
   return (<>
     <Navbar/>
+    <Hero/>
     <div>App</div>
     </>
   )
