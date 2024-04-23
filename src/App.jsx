ae636e6abcee55cc82f8735f284d88c9d3f21841
MAURICEDS0UZA <145907681+MAURICEDS0UZA@users.noreply.github.com>
2024-04-23 17:28:24 +0530

    Feat:HomeCards component into App.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
 import Hero from './Components/Hero'
+import HomeCards from './Components/HomeCards'
 
 export default function App() {
   return (<>
     <Navbar/>
     <Hero/>
+    <HomeCards/>
     <div>App</div>
     </>
   )
