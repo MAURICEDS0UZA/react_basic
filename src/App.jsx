dd85651cc2b3884c4fcb2c7504c3362589e8c310
MAURICEDS0UZA <145907681+MAURICEDS0UZA@users.noreply.github.com>
2024-04-24 21:33:50 +0530

    Add:Job listing component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import Navbar from './Components/Navbar'
-import Hero from './Components/Hero'
-import HomeCards from './Components/HomeCards'
+import React from "react";
+import Navbar from "./Components/Navbar";
+import Hero from "./Components/Hero";
+import HomeCards from "./Components/HomeCards";
+import { JobListings } from "./Components/JobListings";
 
 export default function App() {
-  return (<>
-    <Navbar/>
-    <Hero/>
-    <HomeCards/>
-    <div>App</div>
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <HomeCards />
+      <JobListings />
     </>
-  )
+  );
 }
