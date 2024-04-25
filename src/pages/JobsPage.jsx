fabddea438a9be0bd951bf0bc8a2a6bdd9adec67
MAURICEDS0UZA <145907681+MAURICEDS0UZA@users.noreply.github.com>
2024-04-25 16:16:54 +0530

    New:Job page.

---
+++ src/pages/JobsPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { JobListings } from "../Components/JobListings";
+import ViewAllJobs from "../Components/ViewAllJobs";
+
+export default function JobsPage() {
+  return (
+    <>
+      <JobListings />
+      <ViewAllJobs />
+    </>
+  );
+}
