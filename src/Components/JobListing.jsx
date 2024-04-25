fd3a15c31fa51f5e0d8a0759eea7e30c10a840bf
MAURICEDS0UZA <145907681+MAURICEDS0UZA@users.noreply.github.com>
2024-04-25 12:15:59 +0530

    Feat:Component state updation on click.

--- src/Components/JobListing.jsx
+++ src/Components/JobListing.jsx
@@ -1,23 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { jobs } from "../jobs.json";
 
-
 export const JobListing = () => {
-const requiredId=['1','2','3']
+  const [showDescriptions, setShowDescriptions] = useState({});
+
+  const toggleDescription = (jobId) => {
+    setShowDescriptions((prevDescriptions) => ({
+      ...prevDescriptions,
+      [jobId]: !prevDescriptions[jobId]
+    }));
+  };
+console.log(showDescriptions)
+  const requiredIds = ['1', '2', '3'];
+
   return (
     <>
       {jobs.map((job) => (
-       requiredId.includes(job.id)&&(
-        <div key={job.id} className="container bg-white rounded-2xl px-4 py-6">
-          <h3 className="text-gray-400">{job.type}</h3>
-          <h2 className="font-semibold py-2">{job.title}</h2>
-          <p className="pb-4">{job.description}</p>
-          <h3 className="py-1">{job.salary}</h3>
-          <div className="flex flex-row justify-between border-t-2 pt-4">
-            <div>{job.location}</div>
-            <button className="bg-indigo-500 rounded-md p-2 text-white">Read more</button>
+        requiredIds.includes(job.id) && (
+          <div key={job.id} className="container bg-white rounded-2xl px-4 py-6">
+            <h3 className="text-gray-400">{job.type}</h3>
+            <h2 className="font-semibold py-2">{job.title}</h2>
+            <p className="pb-4">{showDescriptions[job.id] ? job.description : (job.description.substring(0, 100) + "...")}</p>
+            <button onClick={() => toggleDescription(job.id)} className="text-indigo-400 mb-5 hover:text-indigo-600">{showDescriptions[job.id] ? 'Less' : "More"}</button>
+            <h3 className="py-1">{job.salary}</h3>
+            <div className="flex flex-row justify-between border-t-2 pt-4">
+              <div>{job.location}</div>
+              <button className="bg-indigo-500 rounded-md p-2 text-white">Read more</button>
+            </div>
           </div>
-        </div>)
+        )
       ))}
     </>
   );
