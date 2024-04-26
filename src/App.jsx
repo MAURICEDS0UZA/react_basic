8b5074e98e0252f914ec839895ccd06d61eab62b
MAURICEDS0UZA <145907681+MAURICEDS0UZA@users.noreply.github.com>
2024-04-26 17:13:23 +0530

    Feat:Route added to Not found page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import MainLayout from "./layout/MainLayout";
 import JobsPage from "./pages/JobsPage";
+import NotFoundPage from "./Components/NotFoundPage";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Route exact path="/" element={<MainLayout/>}>
           <Route index element={<HomePage />} />
           <Route exact path="/jobs" element={<JobsPage />} />
+          <Route exact path="/*" element={<NotFoundPage />} />
         </Route>
       </Routes>
     </Router>
