b229f91165b4b1fd82463fa7330dc615bd62db10
MAURICEDS0UZA <145907681+MAURICEDS0UZA@users.noreply.github.com>
2024-04-26 16:04:48 +0530

    Remove:Hero component from Main layout.

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
-import Hero from "../Components/Hero";
 import { Outlet } from "react-router-dom";
+
 export default function MainLayout() {
   return (
     <>
       <Navbar />
-      <Hero />
       <Outlet />
     </>
   );
