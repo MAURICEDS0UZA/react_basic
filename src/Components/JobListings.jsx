8738a29f9662eef745c656568a54d6430cbf847b
MAURICEDS0UZA <145907681+MAURICEDS0UZA@users.noreply.github.com>
2024-04-27 17:37:44 +0530

    Feat:Condition rendering of the job.

--- src/Components/JobListings.jsx
+++ src/Components/JobListings.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { JobListing } from "./JobListing";
+import { jobs } from "../jobs.json";
 
-export const JobListings = () => {
+export const JobListings = ({isHome}) => {
   return (
     <section className="bg-indigo-100 px-4 py-10">
       <div className="container-xl lg:container m-auto ">
         <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-          Recent Jobs
+         {isHome?" Recent Jobs":"Browser Jobs"}
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          <JobListing />
+          {jobs.map((job)=>(
+          <JobListing isHome={isHome} job={job}/>
+          ))}
         </div>
       </div>
     </section>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,7 +9,7 @@ export default function HomePage() {
     <>
       <Hero />
       <HomeCards />
-      <JobListings />
+      <JobListings isHome={true}/>
       <ViewAllJobs />
     </>
   );
