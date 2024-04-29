d3e5b06faa5972da3d231d7b4cc854f295ef459c
MAURICEDS0UZA <145907681+MAURICEDS0UZA@users.noreply.github.com>
2024-04-29 12:57:55 +0530

    Feat:Fetching jobs and useEffect.

--- src/Components/JobListing.jsx
+++ src/Components/JobListing.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 let listJob = [];
 
-export const JobListing = ({ job, isHome }) => {
+export const JobListing = ({ job }) => {
   const [showDescriptions, setShowDescriptions] = useState({});
 
   const toggleDescription = (jobId) => {
@@ -15,44 +15,37 @@ export const JobListing = ({ job, isHome }) => {
   };
 
   listJob.push(job.id);
-  function removeDuplicates(listJob) {
-    return listJob.filter((item, index) => listJob.indexOf(item) === index);
-  }
 
-  const requiredIds = isHome? removeDuplicates(listJob).slice(0, 3):removeDuplicates(listJob)
-  
   return (
     <>
-      {requiredIds.includes(job.id) && (
-        <div key={job.id} className="container bg-white rounded-2xl px-4 py-6">
-          <h3 className="text-gray-400">{job.type}</h3>
-          <h2 className="font-semibold py-2">{job.title}</h2>
-          <p className="pb-4">
-            {showDescriptions[job.id]
-              ? job.description
-              : job.description.substring(0, 100) + "..."}
-          </p>
-          <button
-            onClick={() => toggleDescription(job.id)}
-            className="text-indigo-400 mb-5 hover:text-indigo-600"
-          >
-            {showDescriptions[job.id] ? "Less" : "More"}
-          </button>
-          <h3 className="py-1">{job.salary}</h3>
-          <div className="flex flex-row justify-between border-t-2 pt-4">
-            <div className="text-orange-700 mb-3">
-              <FaMapMarker className="inline mb-1 mr-2" />
-              {job.location}
-            </div>
-            <Link
-              to={`/jobs/${job.id}`}
-              className="bg-indigo-500 rounded-md p-2 text-white"
-            >
-              Read more
-            </Link>
+      <div key={job.id} className="container bg-white rounded-2xl px-4 py-6">
+        <h3 className="text-gray-400">{job.type}</h3>
+        <h2 className="font-semibold py-2">{job.title}</h2>
+        <p className="pb-4">
+          {showDescriptions[job.id]
+            ? job.description
+            : job.description.substring(0, 100) + "..."}
+        </p>
+        <button
+          onClick={() => toggleDescription(job.id)}
+          className="text-indigo-400 mb-5 hover:text-indigo-600"
+        >
+          {showDescriptions[job.id] ? "Less" : "More"}
+        </button>
+        <h3 className="py-1">{job.salary}</h3>
+        <div className="flex flex-row justify-between border-t-2 pt-4">
+          <div className="text-orange-700 mb-3">
+            <FaMapMarker className="inline mb-1 mr-2" />
+            {job.location}
           </div>
+          <Link
+            to={`/jobs/${job.id}`}
+            className="bg-indigo-500 rounded-md p-2 text-white"
+          >
+            Read more
+          </Link>
         </div>
-      )}
+      </div>
     </>
   );
 };
--- src/Components/JobListings.jsx
+++ src/Components/JobListings.jsx
@@ -1,17 +1,35 @@
 import React from "react";
 import { JobListing } from "./JobListing";
-import { jobs } from "../jobs.json";
+import { useState, useEffect } from "react";
 
-export const JobListings = ({isHome}) => {
+export const JobListings = ({ isHome = false }) => {
+  const [jobs, setjobs] = useState([]);
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      try {
+        const jobUrl = isHome
+          ? "http://localhost:8000/jobs?_limit=3"
+          : "http://localhost:8000/jobs";
+        const res = await fetch(jobUrl);
+        const data = await res.json();
+        setjobs(data);
+      } catch (error) {
+        console.error("Error found:", error);
+      }
+    };
+    fetchJobs();
+  }, []);
+
   return (
     <section className="bg-indigo-100 px-4 py-10">
       <div className="container-xl lg:container m-auto ">
         <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-         {isHome?" Recent Jobs":"Browser Jobs"}
+          {isHome ? " Recent Jobs" : "Browser Jobs"}
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {jobs.map((job)=>(
-          <JobListing isHome={isHome} job={job} key={job.id}/>
+          {jobs.map((job) => (
+            <JobListing job={job} key={job.id} />
           ))}
         </div>
       </div>
