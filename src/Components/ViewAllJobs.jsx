a9fb79b55cec337b177ca7c86277a04b35d135c0
MAURICEDS0UZA <145907681+MAURICEDS0UZA@users.noreply.github.com>
2024-04-29 13:13:26 +0530

    Feat:conditional rendering for browser jobs .

--- src/Components/ViewAllJobs.jsx
+++ src/Components/ViewAllJobs.jsx
@@ -1,14 +1,23 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-const ViewAllJobs = () => {
+const ViewAllJobs = ({ isHome = false }) => {
   return (
-    <section className='m-auto max-w-lg my-10 px-6'>
-      <Link
-        to='/jobs'
-        className='block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700'
-      >
-        View All Jobs
-      </Link>
+    <section className="m-auto max-w-lg my-10 px-6">
+      {isHome ? (
+        <Link
+          to="/jobs"
+          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+        >
+          View All Jobs
+        </Link>
+      ) : (
+        <Link
+          to="/"
+          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+        >
+          Go Back to Browser Jobs
+        </Link>
+      )}
     </section>
   );
 };
