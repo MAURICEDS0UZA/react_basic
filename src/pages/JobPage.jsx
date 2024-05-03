242b05203fd0681db4eeecceca71e2aae4f181ec
MAURICEDS0UZA <145907681+MAURICEDS0UZA@users.noreply.github.com>
2024-05-03 11:42:33 +0530

    Feat:Added props func Delete job from list

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,8 +1,10 @@
 import { useParams, useLoaderData } from "react-router-dom";
 import { FaArrowLeft, FaMapMarker } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-const JobPage = () => {
+const JobPage = ({ deleteJobList }) => {
+  const navigate = useNavigate();
   const { id } = useParams();
   const job = useLoaderData();
   return (
@@ -65,7 +67,6 @@ const JobPage = () => {
                 <h3 className="text-xl">Contact Phone:</h3>
 
                 <p className="my-2 bg-indigo-100 p-2 font-bold">
-                  {" "}
                   {job.company.contactPhone}
                 </p>
               </div>
@@ -75,7 +76,13 @@ const JobPage = () => {
                 <Link className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
                   Edit Job
                 </Link>
-                <button className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
+                <button
+                  onClick={() => {
+                    deleteJobList(job.id);
+                    navigate("/jobs");
+                  }}
+                  className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
+                >
                   Delete Job
                 </button>
               </div>
