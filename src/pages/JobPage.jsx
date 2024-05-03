95566675adbaae67535964167f60b400b565eb1e
MAURICEDS0UZA <145907681+MAURICEDS0UZA@users.noreply.github.com>
2024-05-03 23:41:31 +0530

    Add: Path to edit btn.

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -73,7 +73,10 @@ const JobPage = ({ deleteJobList }) => {
 
               <div className="bg-white p-6 rounded-lg shadow-md mt-6">
                 <h3 className="text-xl font-bold mb-6">Manage Job</h3>
-                <Link className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
+                <Link
+                  to={`/edit-jobs/${job.id}`}
+                  className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
+                >
                   Edit Job
                 </Link>
                 <button
