6d4a68866c3b0f6109cb249d8bb7f35d08cec789
MAURICEDS0UZA <145907681+MAURICEDS0UZA@users.noreply.github.com>
2024-05-05 22:50:29 +0530

    Feat:React toastify added.

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import Navbar from "../Components/Navbar";
 import { Outlet } from "react-router-dom";
 
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 export default function MainLayout() {
   return (
     <>
       <Navbar />
       <Outlet />
+      <ToastContainer />
     </>
   );
 }
